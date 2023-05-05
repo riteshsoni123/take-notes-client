092ddb540ba212c7e8ade83d242b1534e34cd8b7
riteshsoni123 <comedybuddy1@gmail.com>
2023-05-05 23:12:58 +0530

    made changes in the axios

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,13 @@
+// import axios from "axios";
+// const instance = axios.create({
+//   baseURL: "http://localhost:8000",
+// });
+
+// export default instance;
+
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: `${process.env.REACT_APP_BACKEND_URL}`,
 });
 
 export default instance;
