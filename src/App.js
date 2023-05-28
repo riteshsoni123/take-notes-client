9048b662dbc56d8972dd1fde9acf55fb736469e5
riteshsoni123 <comedybuddy1@gmail.com>
2023-05-28 13:27:24 +0530

    made changes in design of frontend

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PrivateRoute from "./components/routing/PrivateRoute";
 
 // Screens
-// import Login from "./components/screens/Login";
-// import SignUp from "./components/screens/SignUp";
-// import ForgotPassword from "./components/screens/ForgotPassword";
-// import ResetPassword from "./components/screens/ResetPassword";
+import Login from "./components/screens/Login";
+import SignUp from "./components/screens/SignUp";
+import ForgotPassword from "./components/screens/ForgotPassword";
+import ResetPassword from "./components/screens/ResetPassword";
 
 import PrivateScreen from "./components/screens/PrivateScreen";
-import LoginScreen from "./components/screens/LoginScreen";
-import RegisterScreen from "./components/screens/RegisterScreen";
-import ForgotPasswordScreen from "./components/screens/ForgotPasswordScreen";
-import ResetPasswordScreen from "./components/screens/ResetPasswordScreen";
+// import LoginScreen from "./components/screens/LoginScreen";
+// import RegisterScreen from "./components/screens/RegisterScreen";
+// import ForgotPasswordScreen from "./components/screens/ForgotPasswordScreen";
+// import ResetPasswordScreen from "./components/screens/ResetPasswordScreen";
 
 function App() {
   return (
@@ -29,17 +29,13 @@ function App() {
               </PrivateRoute>
             }
           />
-          <Route exact path="/login" element={<LoginScreen />} />
-          <Route exact path="/register" element={<RegisterScreen />} />
-          <Route
-            exact
-            path="/forgotpassword"
-            element={<ForgotPasswordScreen />}
-          />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<SignUp />} />
+          <Route exact path="/forgotpassword" element={<ForgotPassword />} />
           <Route
             exact
             path="/passwordreset/:resetToken"
-            element={<ResetPasswordScreen />}
+            element={<ResetPassword />}
           />
         </Routes>
       </div>
